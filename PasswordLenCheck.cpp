6b2833cdd425cc9c44a9d1f4dda1e880e8215627
EmberCollege <160212205@student.rnngroup.ac.uk>
2022-12-05 08:51:40 +0000

    Added return

--- PasswordLenCheck.cpp
+++ PasswordLenCheck.cpp
@@ -7,11 +7,11 @@ int main(){
     std::cin >> password;
     int passLen = password.size();
 
-    std::cout << passLen << std::endl;
-
     if(passLen <= 8){
         std::cout << "Too Short! Please use a password with eight characters or more!";
     }else if (passLen >= 9){
         std::cout << "\nThis is a good length for a password!";
     }
+    
+    return 0;
 }
